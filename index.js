bd0d681925896311538626406f4ec49c288eca6a
Forest Belton <forest@homolo.gy>
2017-05-04 20:46:20 -0700

    Add 16 more stubs and load CPU with ROM

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 // @flow
 import CPU from './lib/CPU';
+
+fetch('rom.vms')
+    .then(resp => resp.arrayBuffer())
+    .then(buf => {
+        const rom = new Uint8Array(buf);
+        const cpu = new CPU(rom);
+
+        while (true) {
+            cpu.step();
+        }
+    });
